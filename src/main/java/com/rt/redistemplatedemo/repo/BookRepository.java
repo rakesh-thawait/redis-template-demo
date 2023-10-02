45dd6e924e9996f77e2a8e7134b0d125f23b27c5
Rakesh Thawait <Rakesh.Thawait@bluestem.com>
2023-10-01 17:53:31 -0700

    Add new end points

--- src/main/java/com/rt/redistemplatedemo/repo/BookRepository.java
+++ src/main/java/com/rt/redistemplatedemo/repo/BookRepository.java
@@ -4,9 +4,12 @@ import com.rt.redistemplatedemo.entity.Book;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 
 @Repository
 public interface BookRepository extends CrudRepository<Book, String> {
 
+    Book findByName(String name);
 
+    Optional<Book> findByIsbn(String isbn);
 }
